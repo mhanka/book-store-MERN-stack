578fe3ec571e32b77acc1c8f4d445c82c8e75776
Mohan <mohankris95@gmail.com>
2024-05-25 18:18:57 +1000

    Fetch aws cred from secrets part of CI/CD work coming up

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 //Configure the AWS Provider
 provider "aws" {
   //Set the AWS region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region = var.aws_region
-  //Specify the shared credentials file
-  shared_credentials_files = [ "~/.aws/credentials" ]
 }
