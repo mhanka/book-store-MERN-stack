d68f181adddff1ac26da2882817143241374c52b
Mohan <mohankris95@gmail.com>
2024-06-21 20:40:09 +1000

    Configure terraform backend for CI/CD

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,20 @@
 //Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.47.0"
+    }
+  }
+  backend "s3" {
+    bucket         	   = "book-store-terraform-state-bucket"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt = true
+    dynamodb_table = "bookstore_tf_lockid"
+  }
+}
+
 provider "aws" {
   //Set the AWS region
   access_key = var.aws_access_key
